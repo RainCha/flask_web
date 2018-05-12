537958fa0189eb72d9ae704ada63ab5af2121bb9
vinchan <1761211796@qq.com>
2018-05-13 00:58:19 +0800

    添加 jinja 模板

--- manage.py
+++ manage.py
@@ -1,24 +1,26 @@
 # coding=utf-8
+from flask.ext.script import Manager, Server
+from flask.ext.migrate import Migrate, MigrateCommand
 
-# 通过manage.py 运行命令十分必要，因为一些 flask 扩展只有在flask应用对象被创建后才会被初始化，
-# 直接运行默认的python命令行会返回错误
+from main import app, db, User, Post, Tag, Comment
 
-from flask_script import Manager, Server
-from main import app
-
+migrate = Migrate(app, db)
 
 manager = Manager(app)
+manager.add_command("server", Server())
+manager.add_command('db', MigrateCommand)
 
-manager.add_command('server', Server())
 
 @manager.shell
 def make_shell_context():
-  return dict(app=app)
+    return dict(
+        app=app,
+        db=db,
+        User=User,
+        Post=Post,
+        Tag=Tag,
+        Comment=Comment
+    )
 
-
-if __name__ == '__main__':
-  manager.run()
-
-
-# 运行 python manage.py server
-# shell 运行 python manage.py shell
+if __name__ == "__main__":
+    manager.run()
