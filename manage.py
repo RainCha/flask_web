c31162b8cd8f36a0a13fa0a71584e1c894111e79
vinchan <1761211796@qq.com>
2018-05-13 15:45:38 +0800

    重构项目结构

--- manage.py
+++ manage.py
@@ -1,13 +1,21 @@
-# coding=utf-8
-from flask_script import Manager, Server
-from flask_migrate import Migrate, MigrateCommand
+import os
 
-from main import app, db, User, Post, Tag, Comment
+from flask.ext.script import Manager, Server
+from flask.ext.script.commands import ShowUrls
+from flask.ext.migrate import Migrate, MigrateCommand
+
+from webapp import create_app
+from webapp.models import db, User, Post, Tag, Comment
+
+# default to dev config
+env = os.environ.get('WEBAPP_ENV', 'dev')
+app = create_app('webapp.config.%sConfig' % env.capitalize())
 
 migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command("server", Server())
+manager.add_command("show-urls", ShowUrls())
 manager.add_command('db', MigrateCommand)
 
 
